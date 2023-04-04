f63c028060e54d42010adc01f2e379a961461674
applenhi123456 <125206178+applenhi123456@users.noreply.github.com>
2023-04-04 15:08:52 +0700

    Update CTDL&GT_Insertion_Sort_1.cpp

--- CTDL&GT_Insertion_Sort_1.cpp
+++ CTDL&GT_Insertion_Sort_1.cpp
@@ -1,13 +1,20 @@
-//link tham khao:https://duongdinh24.com/thuat-toan-sap-xep-chen/
-void insertionSort(int a[], int n){
-    int j, x;
-    for(int i=1;i<n;i++){
-         x=a[i];
-         j=i;
-        while(j>0 && x<a[j-1]){
-            a[j]=a[j-1];
-            j--;
-        }
-        a[j]=x;
-    }
+//link tham khảo:https://freetuts.net/thuat-toan-sap-xep-chen-insertion-sort-2930.html
+void insertionSort(int arr[], int n)
+{
+   int i, key, j;
+   for (i = 1; i < n; i++)
+   {
+       key = arr[i];
+       j = i-1;
+  
+       /* Di chuyển các phần tử có giá trị lớn hơn giá trị 
+       key về sau một vị trí so với vị trí ban đầu
+       của nó */
+       while (j >= 0 && arr[j] > key)
+       {
+           arr[j+1] = arr[j];
+           j = j-1;
+       }
+       arr[j+1] = key;
+   }
 }
