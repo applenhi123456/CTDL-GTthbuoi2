3338ea35ad9e7d4613866a421913d51fa791c7a0
applenhi123456 <125206178+applenhi123456@users.noreply.github.com>
2023-04-04 14:52:38 +0700

    Update CTDL&GT_Selection_Sort_1.cpp

--- CTDL&GT_Selection_Sort_1.cpp
+++ CTDL&GT_Selection_Sort_1.cpp
@@ -1,24 +1,11 @@
-//link tham khảo:https://gochocit.com/ky-thuat-lap-trinh/thuat-toan-sap-xep-chon-truc-tiep-selection-sort
-void Selection_Sort(int a[], int n){
-	int min;//vị trí phần tử nhỏ nhất trong dãy hiện hành
-	for(int i=0; i<n-1; i++){
-		min = i;
-		for(int j=i+1; j<n; j++){
-			if (a[j] < a[min]){
-				min = j;//ghi nhận vị trí phần tử nhỏ nhất
-			}
-		}
-		swap(a[min], a[i]);
-	}
-}
-
-void main()
+//link tham khảo:https://chidokun.github.io/2015/03/selection-sort/
+int min, i, j; //min là chỉ số phần tử nhỏ nhất
+for(i = 0; i < n-1; i++)
 {
-	int a[5] = {8, 4, 1, 6, 5};
-	Selection_Sort(a, 5);
-	cout<<"Mang sau khi sap xep:"<<endl;
-	for(int i=0;i<5;i++){
-		cout<<a[i]<<" ";
-	}
-	system("pause");
+   min = i;
+   for(j = i+1; j < n; j++)
+      if(a[j] < a[min])
+         min = j; //tìm min trong các phần tử còn lại
+   if(min != i)
+     swap(a[i], a[min]); //đổi chỗ nếu tìm thấy min
 }
